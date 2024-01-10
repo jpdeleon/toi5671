4ab9d816afcc8212d3bc8d2c23a1f20097fd3e59
jpdeleon <jpdeleon.bsap@gmail.com>
2024-01-10 15:22:42 +0900

    update af joint

--- allesfitter/tess/s16s23s50/run.py
+++ allesfitter/tess/s16s23s50/run.py
@@ -4,5 +4,5 @@ import allesfitter
 fig = allesfitter.show_initial_guess('.')
 #allesfitter.prepare_ttv_fit('.')
 
-#allesfitter.ns_fit('.')
-#allesfitter.ns_output('.')
+allesfitter.ns_fit('.')
+allesfitter.ns_output('.')
