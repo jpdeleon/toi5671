b77385da88c29c7d5506af677d97adc710b4d972
jerome de leon <jpdeleon.bsap@gmail.com>
2024-02-24 23:01:44 +0900

    allesfit results

--- allesfitter/all_lc/run.py
+++ allesfitter/all_lc/run.py
@@ -3,8 +3,8 @@ import allesfitter
 
 fig = allesfitter.show_initial_guess('.')
 
-#allesfitter.ns_fit('.')
-#allesfitter.ns_output('.')
+allesfitter.ns_fit('.')
+allesfitter.ns_output('.')
 
 #allesfitter.mcmc_fit('.')
 #allesfitter.mcmc_output('.')
