e5aca71e9afbdc0580d8ad5a9b6b716ec6f55d84
jerome de leon <jpdeleon.bsap@gmail.com>
2024-02-26 09:49:25 +0900

    eccentric fit

--- allesfitter/ttv/run.py
+++ allesfitter/ttv/run.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import allesfitter
 
-fig = allesfitter.prepare_ttv_fit('.')
-#fig = allesfitter.show_initial_guess('.')
-#allesfitter.ns_fit('.')
-#allesfitter.ns_output('.')
+#fig = allesfitter.prepare_ttv_fit('.')
+fig = allesfitter.show_initial_guess('.')
+allesfitter.ns_fit('.')
+allesfitter.ns_output('.')
 
 #allesfitter.mcmc_fit('.')
 #allesfitter.mcmc_output('.')
